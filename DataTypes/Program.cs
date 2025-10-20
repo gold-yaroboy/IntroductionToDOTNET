7cc3f1787973a74417912029ce29098039b600d4
ilyav <ilyav@ILIA>
2025-10-21 00:40:52 +0500

    DataTypes lesson

---
+++ DataTypes/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTypes
+{
+	internal class Program
+	{
+		const string delimeter = "\n-------------------------------------------------------------------\n";
+		static void Main(string[] args)
+		{
+			Console.WriteLine($"- bool is {sizeof(bool)} bytes, class Wrapper: {typeof(bool)}");
+			Console.WriteLine(delimeter);
+			Console.WriteLine($"- char is {sizeof(char)} bytes, range values: {(int)char.MinValue} - {(int)char.MaxValue}, class Wrapper: {typeof(char)}");
+			Console.WriteLine(delimeter);
+			Console.WriteLine($"- byte is {sizeof(byte)} bytes, range values: {byte.MinValue} - {byte.MaxValue}, class Wrapper: {typeof(byte)}");
+			Console.WriteLine(delimeter);
+			Console.WriteLine($"- sbyte is {sizeof(sbyte)} bytes, range values: {sbyte.MinValue} - {sbyte.MaxValue}, class Wrapper: {typeof(sbyte)}");
+			Console.WriteLine(delimeter);
+
+			bool rain = true;
+			Console.WriteLine($"Rain go? - {rain}");
+		}
+	}
+}
